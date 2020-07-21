ba64b81dd46f4b2d63144f09405e120266d02114
Andrew Kennedy <akennedycode@gmail.com>
2020-07-21 00:54:38 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -7,3 +7,8 @@ function updatePlaylist(playlist, artist, song) {
   playlist [artist] = song
   return playlist
 }
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist [artist]
+  return playlist
+}
