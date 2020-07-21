b8e162b37adb0d519173e26d96aba6c4b2182e0e
Andrew Kennedy <akennedycode@gmail.com>
2020-07-21 00:52:04 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,3 +2,8 @@ var playlist = {
   Sublime: 'What I Got',
   'Third Eye Blind': 'Semi-Charmed Life'
 }
+
+function updatePlaylist(playlist, artist, song) {
+  playlist [artist] = song
+  return playlist
+}
